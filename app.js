d3e38c6bf78d5e7227695142ae9d7b10e9a13c55
harshal patel <41120010+harshalpat@users.noreply.github.com>
2021-04-11 22:14:21 +0200

    passport-local-mongoose-authentication

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var session = require('express-session');
 var FileStore = require('session-file-store')(session)
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -48,29 +50,23 @@ app.use(session({
 
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+require('./authenticate');
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
 
-  if (!req.session.user) {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');              
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');             
     err.status = 401;
     next(err);
     return;
     }
   else {
-      if (req.session.user === 'authenticated') {
-        console.log('signed cookie exists')
-        next();
-      }
-      else {
-        var err = new Error('You are not authenticated!');
-        err.status = 403;
-        next(err);
-      }
+    next();
   }
 }
 app.use(auth);
--- models/user.js
+++ models/user.js
@@ -1,20 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
-var userSchema = new Schema({
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+var User = new Schema({
     admin: {
         type: Boolean,
         default: false
     }
 });
 
-module.exports = mongoose.model('User', userSchema);
+User.plugin(passportLocalMongoose); //support username, hashed password storage
+
+module.exports = mongoose.model('User', User);
